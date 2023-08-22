51ea3a0508f9a026c009ed99b7397981483f5885
Hank Liu <hankliu@coolbitx.com>
2023-08-22 13:24:41 +0800

    add low quality and gray scale code

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,17 @@ using namespace std;
 
 int main(int, char**) {
     cout << "Hello, World!" << endl;
-    Mat img = imread("./resources/jojo.jpeg", -1);
-
+    Mat img = imread("./resources/jojo.jpeg", ImreadModes::IMREAD_UNCHANGED);
+    Mat gray = imread("./resources/jojo.jpeg", ImreadModes::IMREAD_GRAYSCALE);
+    // imshow("test", img); // not working on MacOS
     vector<int>compress_params;
+    int quality = 5;
+    compress_params.push_back(ImwriteFlags::IMWRITE_JPEG_QUALITY);
+    compress_params.push_back(quality);
 
-    imwrite("./output/newjojo1.jpg", img);
+    imwrite("./output/newjojo.jpg", img);
+    imwrite("./output/grayjojo.jpg", gray);
+    imwrite("./output/lowjojo.jpg", img, compress_params);
 
     cout << img.channels() << endl;
     return 0;
